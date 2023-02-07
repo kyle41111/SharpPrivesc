6f5919db5242c1ca3af15e16b94a75047ddd5667
kyle <kyle41111@gmail.com>
2023-02-07 15:05:35 -0500

    Create Info.cs

---
+++ real/Info.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace Privesc2.Check
+{
+    public static class Info
+    {
+        public static void ShowLogo()
+        {
+
+
+
+
+
+            Console.WriteLine("\r\n ❤             ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣄⠀⠀⠀⠀⠀⠀⠀⢠⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀\n");
+            Console.WriteLine("\r\n ❤ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⢋⣳⢀⣀⣀⣀⣈⣙⣯⡓⠲⢤⣀⠀⠀⠀⠀⢀⣠⢶⣯⣿⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡏⣈⡷⠒⠋⠉⠉⠀⠀⠀⠀⠀⠉⠙⠂⠀⠉⠳⣆⡤⠴⠛⢡⠎⢸⣻⠀⠀      Youre doing it Kupo! You're hacking!  ❤  ❤⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡾⠟⢁⡀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠁⠀⣠⠴⢃⣰⡟⡟⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⣀⣀⠀⠀⠀⠀⠀⠀⢀⡴⠟⢿⣿⠟⠁⢠⠏⠀⠀⠀⠀⠀⠀⣠⠆⠀⣸⠇⠀⠀⠀⠠⢾⣿⣛⡏⣽⠁⠀⠀\n");
+            Console.WriteLine("\r\n⢀⣄⣊⠉⠀⠀⠀⠀⠀⠋⠀⣠⠞⠁⠀⣰⠃⠀⠀⠀⠀⠀⣠⢞⡏⠀⡴⠛⢧⠀⠀⠀⠀⠈⠙⢾⡿⠃⠀⠀⠀⠀\n");
+            Console.WriteLine("\r\n⢠⣿⠍⠀⠀⠀⠀⠀⢀⡴⢎⡡⠖⠀⣴⠁⡴⠶⢶⢀⡴⠋⠁⣼⢠⣾⣷⣶⣾⣷⠀⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⢀⣤⣴⠾⠛⣺⠋⠀⠀⣸⠁⠈⣠⣶⣶⡏⠀⠀⣸⣿⣻⣾⣿⣷⣶⣜⡆⠀⠀⠀⠀⠀⠀⢷⠀⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⡰⠃⣰⠁⢰⠇⠀⣼⣿⠿⣿⣷⠇⣰⠟⠁⢩⡏⠿⣿⣷⠝⢷⠀⡇⠀⠀⠀⠰⣞⡆⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⢸⠃⢠⡇⠀⡏⠀⣸⣟⢹⣿⣷⡿⠰⠋⠀⠀⠸⣿⣿⣿⡿⠀⣾⢠⡇⠀⠀⠀⠀⣿⠿⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠠⡏⣰⢧⠃⣸⠁⣸⡇⠙⠳⠎⠉⠤⠇⠀⠀⠸⠦⣜⠀⠀⠀⡚⢹⣾⡇⠀⠀⡄⢠⣿⠀⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⢸⢷⠏⢸⠀⡿⡇⡿⠉⠉⠉⠀⠀⣦⣠⠴⠶⢤⣠⣼⠃⠈⠟⠋⢸⡏⣧⠀⢠⠇⡼⣿⠀⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⢸⡏⠀⢸⢀⢷⠃⢻⡀⠀⠀⠀⠀⠘⡆⠀⠀⠀⠀⣸⠀⠀⠀⠀⠉⠀⡇⣰⡟⣴⠃⠻⠄⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠘⠁⠀⠘⣿⣼⡇⠈⢳⡄⠀⠀⠀⠀⢳⡀⠀⠀⢠⠇⠀⠀⠀⠀⠀⢰⢣⣟⡼⠃⠀⠀⣴⣦⢶⡆\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣏⡏⢧⢀⣠⣽⡷⣄⡀⠀⠈⠁⠀⠐⠋⠀⠀⣀⣄⣠⢴⣿⡿⣞⠀⠀⠀⣼⠷⡇⣼⠁\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⣸⢏⢿⣣⠳⡺⠿⣗⡦⢤⣤⢤⣤⣤⣾⣧⣼⢯⡛⢿⣶⡃⠀⠀⢠⣿⡀⠛⣿⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣧⣨⢿⢻⣣⠱⡐⣼⣇⣼⣿⢿⡄⢀⡿⢯⡫⣳⡳⢄⡇⢧⠀⠀⠘⣎⡇⡸⠃⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⠛⣯⣧⢻⣧⣸⡼⢻⠉⢹⠉⢳⠾⡷⣆⠳⡜⢠⡾⡴⢸⡆⠀⠀⠈⢹⡇⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣎⡴⠋⠑⠒⠭⣿⠁⠀⢒⣾⣶⣶⡤⠽⢿⣧⣼⡿⠟⠳⣼⡇⠀⠀⠀⢸⡇⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⣟⠁⠀⠀⠀⢸⠋⢫⠉⠉⣿⠀⠀⠀⣠⠛⢻⠉⠀⠀⠀⢹⡟⢦⠀⠀⣼⡇⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠘⠦⣄⣀⣠⠟⠀⡎⢇⠀⣿⠀⠀⡼⠁⠀⠘⡆⠀⠀⠀⢸⡇⢸⠀⣰⣿⡇⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣦⠀⠀⠀⠀⠀⣀⠀⠇⢘⣆⣿⠀⠀⠀⠀⣠⣤⣁⡀⢒⠚⠋⠀⠀⠀⠁⠛⠀⠀⠀\n");
+            Console.WriteLine("\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⠻⠿⠿⠟⠛⠹⠿⠿⠇⠻⠿⠸⠿⠿⠛⠿⠿⠿⠁⠀⠀⠀⠀⠀\n");
+
+
+        }
+
+        public static void ShowUsage()
+        {
+            string usage = @"Check for nested groups, and your group members:
+allinone.exe nested <group> <domain>
+
+Check for unquoted service paths: 
+allinone.exe unquoted 
+
+Check for DCsync targets:
+allinone.exe dcsync";
+
+            Console.WriteLine(usage);
+        }
+    }
+}
