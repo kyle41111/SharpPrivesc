f250cb89a47ff9d98b9827cb0b1de24382c17772
kyle <kyle41111@gmail.com>
2023-02-07 15:05:48 -0500

    Create nested.cs

---
+++ real/nested.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Win32;
+using System.ServiceProcess;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
+using System.Security.Principal;
+using System.Security.AccessControl;
+using System.Collections;
+using System.Threading;
+using System.DirectoryServices;
+using GetGroup;
+using Privesc.Commands;
+using Privesc2.Check;
+
+
+namespace Privesc.Commands
+{
+    public class nested : ICommand
+    {
+        public static string CommandName => "nested";
+
+        public void Execute(Dictionary<string, string> arguments)
+        {
+            Console.WriteLine("checking for nested groups");
+
+
+            
+            string group = arguments["/group"];
+            string domain = arguments["/domain"];
+
+            Class2 pp = new Class2();
+            pp.GetAllMembers(group, domain);
+
+
+            
+
+        }
+
+    }
+}
+
+       
